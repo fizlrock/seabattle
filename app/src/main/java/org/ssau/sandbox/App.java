a12a39647dfce3b08bb4b75754797f9cc310b5ad
fizlrock <nirku0soft@yandex.ru>
2024-12-03 23:52:37 +0400

    delete liquibase from gradle

--- app/src/main/java/org/ssau/sandbox/App.java
+++ app/src/main/java/org/ssau/sandbox/App.java
@@ -1,57 +1,18 @@
-/*
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-package org.ssau.sandbox;
 
-import java.util.Map.Entry;
-import java.util.StringJoiner;
-import java.util.stream.Collectors;
+package org.ssau.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import lombok.extern.slf4j.Slf4j;
 
-@SpringBootApplication(exclude = { OAuth2ClientAutoConfiguration.class, SecurityAutoConfiguration.class,
-    ReactiveSecurityAutoConfiguration.class })
+@SpringBootApplication()
 @Slf4j
 public class App {
   public static void main(String[] args) {
     var context = SpringApplication.run(App.class, args);
 
-    findAndPrintBeans(context, SecurityWebFilterChain.class);
   }
 
-  static <T> void findAndPrintBeans(ConfigurableApplicationContext context, Class<T> clazz) {
-
-    var beans = context.getBeansOfType(clazz);
-
-	   var report = beans.entrySet().stream()
-        .map(Entry::getValue)
-        .map(Object::toString)
-        .collect(Collectors.joining("\n"));
-    log.info("Bean type {} report: \n{}", clazz, report);
-
-  }
+  
 }
