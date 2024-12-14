e4df52a66556bb0c353bdc32464a3fa075e43afc
fizlrock <nirku0soft@yandex.ru>
2024-12-14 17:27:05 +0400

    change collection to concurrent

--- app/src/main/java/org/ssau/sandbox/domain/game/GameSessionPool.java
+++ app/src/main/java/org/ssau/sandbox/domain/game/GameSessionPool.java
@@ -1,16 +1,17 @@
 package org.ssau.sandbox.domain.game;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.ssau.sandbox.domain.game.GameSession.GameState;
 
+import io.netty.util.internal.ConcurrentSet;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -21,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GameSessionPool {
 
-  public final Set<GameSession> sessions = new HashSet<>();
+  public final Map<GameSession, Object> sessions = new ConcurrentHashMap<>();
 
   /**
    * Получить статистику пула игровых сессий
@@ -29,7 +30,7 @@ public class GameSessionPool {
    * @return словарь состояние:кол-во
    */
   private Map<GameState, Long> getPoolStateStat() {
-    return sessions.stream()
+    return sessions.entrySet().stream().map(Entry::getKey)
         .collect(Collectors.groupingBy(GameSession::getState, Collectors.counting()));
   }
 
@@ -48,14 +49,14 @@ public class GameSessionPool {
 
   private GameSession createNewSession() {
     var session = new GameSession(settings);
-    sessions.add(session);
+    sessions.put(session, null);
     logPoolState();
     return session;
   }
 
   public GameSession findSession() {
     log.debug("Поиск игровой сессии");
-    var session = sessions.stream()
+    var session = sessions.entrySet().stream().map(Entry::getKey)
         .filter(s -> s.getState() == GameState.WaitingSecondPlayer)
         .findAny();
 
